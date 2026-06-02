d24ce685847673a465fbe2fa44885253e0d4c5b4
Sanjai PS <sanjaips.tech@gmail.com>
2026-06-02 21:16:25 +0530

    Add variables and datatypes lesson 1

---
+++ Variables and Datatype/test.java
@@ -0,0 +1,9 @@
+class test {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "Hello";
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
