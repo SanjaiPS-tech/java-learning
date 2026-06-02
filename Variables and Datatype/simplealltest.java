4123e93924c3d169da648a03c6921168e9decb7f
Sanjai PS <sanjaips.tech@gmail.com>
2026-06-02 21:25:43 +0530

    Add variables and datatypes lesson 2

---
+++ Variables and Datatype/simplealltest.java
@@ -0,0 +1,9 @@
+public class simplealltest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+
+        int c = a + b;
+        System.out.println(c);
+    }
+}
