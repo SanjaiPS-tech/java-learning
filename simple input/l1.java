c30bf61f9c28f92e5447b08c1d70945f1da1fda7
Sanjai PS <sanjaips.tech@gmail.com>
2026-06-02 22:31:54 +0530

    lesson input

---
+++ simple input/l1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class l1 {
+    public static void main(String[] args) {
+        Scanner test = new Scanner(System.in);
+        int a = test.nextInt();
+        int b = test.nextInt();
+
+        int c = a + b;
+        System.out.println(c);
+        test.close();
+    }
+}
+
