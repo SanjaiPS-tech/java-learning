fd14fada017e89fa2bec4c6a1ac93f75d560f365
Sanjai PS <sanjaips.tech@gmail.com>
2026-06-04 12:09:18 +0530

    Example on college

--- Demo.java
+++ Demo.java
@@ -4,19 +4,18 @@ public class Demo {
     public static void main(String args[]) {
 
         Scanner in = new Scanner(System.in);
+
         System.out.print("Enter Your Name : ");
         String sName = in.next();
 
-
-
-        
         System.out.print("Enter Your Age : ");
         String sAge = in.next();
 
-
-
         System.out.print("Enter Your Dept : ");
         String sDept = in.next();
+
+        System.out.print("Enter Your CGPA : ");
+        String sCgpa = in.next();
 
         System.out.print("Name : ");
         System.out.println(sName);
@@ -24,6 +23,8 @@ public class Demo {
         System.out.println(sAge);
         System.out.print("Dept : ");
         System.out.println(sDept);
+        System.out.print("CGPA : ");
+        System.out.println(sCgpa);
 
 
     }
